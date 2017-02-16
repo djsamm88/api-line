895d36076fdb01dbf09f7050121eb87ac7ec2653
djsamm88 <djsamm88.web.id@gmail.com>
2017-02-17 02:36:11 +0700

    Add files via upload

---
+++ bot.php
@@ -0,0 +1,218 @@
+<?php
+/*
+copyright @ medantechno.com
+2017
+
+*/
+
+require_once('./line_class.php');
+
+$channelAccessToken = ''; //sesuaikan 
+$channelSecret = '';//sesuaikan
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+
+//var_dump($client->parseEvents());
+
+
+
+//$_SESSION['userId']=$client->parseEvents()[0]['source']['userId'];
+
+/*
+{
+  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+  "type": "message",
+  "timestamp": 1462629479859,
+  "source": {
+    "type": "user",
+    "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+  },
+  "message": {
+    "id": "325708",
+    "type": "text",
+    "text": "Hello, world"
+  }
+}
+*/
+
+
+$userId 	= $client->parseEvents()[0]['source']['userId'];
+$replyToken = $client->parseEvents()[0]['replyToken'];
+$timestamp	= $client->parseEvents()[0]['timestamp'];
+
+
+$message 	= $client->parseEvents()[0]['message'];
+$messageid 	= $client->parseEvents()[0]['message']['id'];
+
+$profil = $client->profil($userId);
+
+$pesan_datang = $message['text'];
+
+
+
+//pesan bergambar
+if($message['type']=='text')
+{
+	if($pesan_datang=='1')
+	{
+		
+		
+		$balas = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+								array(
+										'type' => 'text',					
+										'text' => 'Halo '.$profil->displayName.', Anda memilih menu 1,'
+									)
+							)
+						);
+				
+	}
+	else
+	if($pesan_datang=='2')
+	{
+		$get_sub = array();
+		$aa =   array(
+						'type' => 'image',									
+						'originalContentUrl' => 'https://medantechno.com/line/images/bolt/1000.jpg',
+						'previewImageUrl' => 'https://medantechno.com/line/images/bolt/240.jpg'	
+						
+					);
+		array_push($get_sub,$aa);	
+
+		$get_sub[] = array(
+									'type' => 'text',									
+									'text' => 'Halo '.$profil->displayName.', Anda memilih menu 2, harusnya gambar muncul.'
+								);
+		
+		$balas = array(
+					'replyToken' 	=> $replyToken,														
+					'messages' 		=> $get_sub
+				 );	
+		/*
+		$alt = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+								array(
+										'type' => 'text',					
+										'text' => 'Anda memilih menu 2, harusnya gambar muncul.'
+									)
+							)
+						);
+		*/
+		//$client->replyMessage($alt);
+	}
+	else
+	if($pesan_datang=='3')
+	{
+		
+		$balas = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+								array(
+										'type' => 'text',					
+										'text' => 'Fungsi PHP base64_encode medantechno.com :'. base64_encode("medantechno.com")
+									)
+							)
+						);
+				
+	}
+	else
+	if($pesan_datang=='4')
+	{
+		
+		$balas = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+								array(
+										'type' => 'text',					
+										'text' => 'Jam Server Saya : '. date('Y-m-d H:i:s')
+									)
+							)
+						);
+				
+	}
+	else
+	if($pesan_datang=='6')
+	{
+		
+		$balas = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+								array(
+										'type' => 'location',					
+										'title' => 'Lokasi Saya.. Klik Detail',					
+										'address' => 'Medan',					
+										'latitude' => '3.521892',					
+										'longitude' => '98.623596' 
+									)
+							)
+						);
+				
+	}
+	else
+	if($pesan_datang=='7')
+	{
+		
+		$balas = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+								array(
+										'type' => 'text',					
+										'text' => 'Testing PUSH pesan ke anda'
+									)
+							)
+						);
+						
+		$push = array(
+							'to' => $userId,									
+							'messages' => array(
+								array(
+										'type' => 'text',					
+										'text' => 'Pesan ini dari medantechno.com'
+									)
+							)
+						);
+						
+		
+		$client->pushMessage($push);
+				
+	}
+
+	else{
+
+		$balas = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+								array(
+										'type' => 'text',					
+										'text' => 'Halo.. Selamat datang di medantechno.com .        Untuk testing menu pilih 1,2,3,4,5 ... atau stiker'
+									)
+							)
+						);
+						
+	}
+
+}else if($message['type']=='sticker')
+{	
+	$balas = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+								array(
+										'type' => 'text',									
+										'text' => 'Terimakasih stikernya... '										
+									
+									)
+							)
+						);
+						
+}
+ 
+$result =  json_encode($balas);
+//$result = ob_get_clean();
+
+file_put_contents('./balasan.json',$result);
+
+
+$client->replyMessage($balas);
+
---
+++ line_class.php
@@ -0,0 +1,186 @@
+<?php 
+if (!function_exists('hash_equals')) 
+{
+    defined('USE_MB_STRING') or define('USE_MB_STRING', function_exists('mb_strlen'));
+
+    function hash_equals($knownString, $userString)
+    {
+        $strlen = function ($string) {
+            if (USE_MB_STRING) {
+                return mb_strlen($string, '8bit');
+            }
+
+            return strlen($string);
+        };
+
+        if (($length = $strlen($knownString)) !== $strlen($userString)) {
+            return false;
+        }
+
+        $diff = 0;
+
+        for ($i = 0; $i < $length; $i++) {
+            $diff |= ord($knownString[$i]) ^ ord($userString[$i]);
+        }
+        return $diff === 0;
+    }
+}
+
+class LINEBotTiny
+{
+    public function __construct($channelAccessToken, $channelSecret)
+    {
+        $this->channelAccessToken = $channelAccessToken;
+        $this->channelSecret = $channelSecret;
+    }
+	
+	
+
+
+    public function parseEvents()
+    {
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            http_response_code(405);
+            error_log("Method not allowed");
+            exit();
+        }
+
+        $entityBody = file_get_contents('php://input');
+
+        if (strlen($entityBody) === 0) {
+            http_response_code(400);
+            error_log("Missing request body");
+            exit();
+        }
+
+        if (!hash_equals($this->sign($entityBody), $_SERVER['HTTP_X_LINE_SIGNATURE'])) {
+            http_response_code(400);
+            error_log("Invalid signature value");
+            exit();
+        }
+
+        $data = json_decode($entityBody, true);
+        if (!isset($data['events'])) {
+            http_response_code(400);
+            error_log("Invalid request body: missing events property");
+            exit();
+        }
+        return $data['events'];
+    }
+
+    public function replyMessage($message)
+    {
+        $header = array(
+            "Content-Type: application/json",
+            'Authorization: Bearer ' . $this->channelAccessToken,
+        );
+
+        $context = stream_context_create(array(
+            "http" => array(
+                "method" => "POST",
+                "header" => implode("\r\n", $header),
+                "content" => json_encode($message),
+            ),
+        ));
+		$response = exec_url('https://api.line.me/v2/bot/message/reply',$this->channelAccessToken,json_encode($message));
+    }
+	
+    public function pushMessage($message) 
+    {
+        
+		$response = exec_url('https://api.line.me/v2/bot/message/push',$this->channelAccessToken,json_encode($message));
+       
+    }
+	
+    public function profil($userId)
+    {
+      
+		return json_decode(exec_get('https://api.line.me/v2/bot/profile/'.$userId,$this->channelAccessToken));
+       
+    }
+
+    private function sign($body)
+    {
+        $hash = hash_hmac('sha256', $body, $this->channelSecret, true);
+        $signature = base64_encode($hash);
+        return $signature;
+    }
+}
+
+
+
+
+
+
+
+function exec_get($fullurl,$channelAccessToken)
+{
+		
+		$header = array(
+            "Content-Type: application/json",
+            'Authorization: Bearer '.$channelAccessToken,
+        );
+
+		
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_VERBOSE, 1);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);		
+		curl_setopt($ch, CURLOPT_FAILONERROR, 0);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+		curl_setopt($ch, CURLOPT_URL, $fullurl);
+		
+		$returned =  curl_exec($ch);
+	
+		return($returned);
+}
+
+
+
+function exec_url($fullurl,$channelAccessToken,$message)
+{
+		
+		$header = array(
+            "Content-Type: application/json",
+            'Authorization: Bearer '.$channelAccessToken,
+        );
+
+		
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_VERBOSE, 1);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+		curl_setopt($ch, CURLOPT_POST,           1 );
+		curl_setopt($ch, CURLOPT_POSTFIELDS,     $message); 
+		curl_setopt($ch, CURLOPT_FAILONERROR, 0);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+		curl_setopt($ch, CURLOPT_URL, $fullurl);
+		
+		$returned =  curl_exec($ch);
+	
+		return($returned);
+}
+
+
+
+function exec_url_aja($fullurl)
+	{
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_HEADER, 0);
+			curl_setopt($ch, CURLOPT_VERBOSE, 1);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+			curl_setopt($ch, CURLOPT_FAILONERROR, 0);
+			curl_setopt($ch, CURLOPT_URL, $fullurl);
+			
+			$returned =  curl_exec($ch);
+		
+			return($returned);
+	}
+	
+
